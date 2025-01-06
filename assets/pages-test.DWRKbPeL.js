414e15babad2fa4f2f0a7658c768bc11a4d54206
MaiYaoqiang <MaiYaoqiang@users.noreply.github.com>
2025-01-06 02:59:31 +0000

    deploy: 61fc03806c43e939fc525ac80697ee41a43bc216

---
+++ assets/pages-test.DWRKbPeL.js
@@ -0,0 +1,1 @@
+const _sfc_main={__name:"test",setup(__props){const str="aaa + 555".replace(/([a-z_]+)/gi,(a=>"${"+a+"}"));return console.log(eval("`"+str+"`")),(a,s)=>null}};export{_sfc_main as default};
